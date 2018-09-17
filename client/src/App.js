5bedd65cfed0351cc0998869ae98a987adc9e5fa
iainkirkpatrick <kirkpatrick.iain@gmail.com>
2018-09-17 16:37:05 +1200

    basic token auth with signin and signup

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ class App extends Component {
   constructor () {
     super()
     this.state = {
+      signedIn: false,
       people: []
     }
   }
@@ -22,6 +23,44 @@ class App extends Component {
     })
   }
   
+  handleSignIn () {
+    const email = document.getElementById('email').value
+    const password = document.getElementById('password').value
+    window.fetch('/api/auth/sign_in', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({ email, password })
+    })
+    .then((res) => {
+      return res.json()
+    })
+    .then((creds) => {
+      console.log('creds', creds)
+    })
+  }
+  
+  handleSignUp () {
+    const email = document.getElementById('email').value
+    const password = document.getElementById('password').value
+    window.fetch('/api/auth', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({ email, password })
+    })
+    .then((res) => {
+      return res.json()
+    })
+    .then((creds) => {
+      console.log('creds', creds)
+    })
+  }
+  
   render() {
     return (
       <div className="App">
@@ -29,14 +68,25 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        {
-          this.state.people.map((person, i) => {
-            return <h2 key={i}>{person.name}</h2>
-          })
-        }
+        <div>
+          <label>email</label>
+          <input id='email'></input>
+        </div>
+        <div>
+          <label>password</label>
+          <input id='password' type={'password'}></input>
+        </div>
+        <button onClick={() => this.handleSignIn()}>sign in</button>
+        <button onClick={() => this.handleSignUp()}>sign up</button>
       </div>
     );
   }
 }
 
 export default App;
+// 
+// {
+//   this.state.people.map((person, i) => {
+//     return <h2 key={i}>{person.name}</h2>
+//   })
+// }
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   namespace :api do
+    mount_devise_token_auth_for 'User', at: 'auth'
     resources :people
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
